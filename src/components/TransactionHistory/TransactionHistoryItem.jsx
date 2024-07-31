d573eb8c66ff4c6d7eab8b1a89a86b5387f6f480
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-07-31 07:38:39 +0200

    i hope so

--- src/components/TransactionHistory/TransactionHistoryItem.jsx
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -3,11 +3,11 @@ import s from "./TransactionHistory.module.css";
 
 const TransactionHistoryItem = ({ type, amount, currency }) => {
   return (
-    <>
+    <tr>
       <td className={clsx(s.firstCol)}>{type}</td>
       <td>{amount}</td>
       <td>{currency}</td>
-    </>
+    </tr>
   );
 };
 
